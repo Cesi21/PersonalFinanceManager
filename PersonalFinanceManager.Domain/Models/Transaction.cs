9c862383c94710c7dd5161a7cb86b84b7260a1db
CesarMitja <77234175+CesarMitja@users.noreply.github.com>
2025-05-19 09:20:33 +0200

    Dodani razredi podatkov in repozitory

---
+++ PersonalFinanceManager.Domain/Models/Transaction.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersonalFinanceManager.Domain.Models
+{
+    public class Transaction
+    {
+        public Guid Id { get; set; } = Guid.NewGuid();
+        public DateTime Date { get; set; }
+        public Category Category { get; set; }
+        public string Description { get; set; } = string.Empty;
+        public decimal Amount { get; set; }
+        public TransactionType Type { get; set; }
+    }
+}
---
+++ PersonalFinanceManager.Domain/Repositories/ITransactionRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PersonalFinanceManager.Domain.Models;
+
+namespace PersonalFinanceManager.Domain.Repositories
+{
+    public interface ITransactionRepository
+    {
+        IEnumerable<Transaction> GetAll();
+        Transaction? GetById(Guid id);
+        void Add(Transaction transaction);
+        void Update(Transaction transaction);
+        void Delete(Guid id);
+    }
+}
